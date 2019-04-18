285f37440c835e8aacc74bc8710456a1943bc796
Dominic Duffin <dominicduffin@outlook.com>
2019-04-18 16:59:33 +0100

    Add files I've worked on on e-ink (no git yet!).

---
+++ circlesAroundHexagon.py
@@ -0,0 +1,10 @@
+﻿from turtle import *
+hideturtle()
+for x in range(6):
+    pen(pencolor="#3300aa", pensize=4)
+    right(60)
+    forward(45)
+    pen(pencolor="#bb00ee", pensize=4)
+    for y in range(360):
+        right(1)
+        forward(1)
---
+++ hexagonsAroundSpiral.py
@@ -0,0 +1,13 @@
+﻿from turtle import *
+hideturtle()
+i = 0.5
+for x in range(36):
+    pen(pencolor="#883300", pensize=2)
+    for y in range(60):
+        right(1)
+        forward(i)
+    pen(pencolor="#cc00ff", pensize=2)
+    for z in range(6):
+        right(60)
+        forward(25)
+    i = i + 0.1
