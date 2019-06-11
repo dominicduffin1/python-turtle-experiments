2b423cf83d59bfaa37aa41e80ec69e6eda6768cd
Dominic Duffin <dominicduffin@outlook.com>
2019-06-11 20:10:02 +0100

    Add blue straight line, green diamond and yellow zigzag functions.

--- generativeLines.py
+++ generativeLines.py
@@ -3,7 +3,7 @@ from turtle import *
 #This program is WIP
 
 def redWave(l):
-    "Draws a red wave. Args: number of iterations of wave."
+    "Draws a red wave. Args: Number of iterations of wave."
     pencolor("#ff0000")
     for waves in range(l):
         for turns in range(180):
@@ -12,5 +12,32 @@ def redWave(l):
         for turns in range(180):
             forward(0.1)
             left(1)
+
+def blueStraightLine(l):
+    "Draws a blue straight line. Args: Length of line."
+    pencolor("#0000ff")
+    for pixels in range(l):
+        forward(1)
+
+def greenDiamond(s):
+    "Draws a green diamond. Args: Size of diamond, s=length of one side."
+    pencolor("#00ff00")
+    forward(s)
+    right(150)
+    forward(s)
+    right(30)
+    forward(s)
+    right(150)
+    forward(s)
+
+def yellowZigZag(l):
+    "Draws a yellow zigzag. Args: Number of iterations of zigzag."
+    pencolor("#ffff00") #Color untested
+    for zees in range(l):
+        forward(12)
+        right(143) #Approx sin^-1 (9/15)
+        forward(15)
+        left(143)
+        forward(12)
 
     
